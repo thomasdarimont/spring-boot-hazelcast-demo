6ae334c421f6d1e99940049eee7c4046320ca54f
Thomas Darimont <thomas.darimont@gmail.com>
2018-04-20 14:34:35 +0200

    Revise example

--- src/main/java/demo/DemoController.java
+++ src/main/java/demo/DemoController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 import java.net.Inet4Address;
+import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,39 +20,54 @@ class DemoController {
     private final HttpSession session;
 
     @GetMapping
-    Object getData() throws Exception {
-        Map<String, String> data = (Map<String, String>) session.getAttribute("data");
-
-        if (data != null) {
-            data.put("node", Inet4Address.getLocalHost().getHostAddress());
-        }
-
-        return data;
+    Object getData() {
+        return toResponse(loadDataInternal());
     }
 
     @GetMapping("{key}/{value}")
     Object putData(@PathVariable String key, @PathVariable String value) {
-        Map<String, String> data = (Map<String, String>) session.getAttribute("data");
-        if (data == null) {
-            data = new HashMap<>();
-        }
+
+        Map<String, String> data = loadDataInternal();
 
         data.put(key, value);
+        saveDataInternal(data);
 
-        session.setAttribute("data", data);
-        return data;
+        return toResponse(data);
     }
 
     @GetMapping(path = "{key}", params = "remove")
     Object removeData(@PathVariable String key) {
-        Map<String, String> data = (Map<String, String>) session.getAttribute("data");
 
-        if (data == null) {
-            return data;
-        }
+        Map<String, String> data = loadDataInternal();
 
         data.remove(key);
+        saveDataInternal(data);
+
+        return toResponse(data);
+    }
+
+    private Map<String, String> loadDataInternal() {
+        Map<String, String> data = (Map<String, String>) session.getAttribute("data");
+        if (data == null) {
+            data = new HashMap<>();
+        }
+        return data;
+    }
+
+    private void saveDataInternal(Map<String, String> data) {
+        // ignore conccurent updates for demo purposes
         session.setAttribute("data", data);
+    }
+
+    private Map<String, String> toResponse(Map<String, String> data) {
+
+        if (data != null) {
+            try {
+                data.put("node", Inet4Address.getLocalHost().getHostAddress());
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            }
+        }
 
         return data;
     }
